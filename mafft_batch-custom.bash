eea6c8cb5b8f59640e94344dd2e14f8f0ba4388f
Ryohei Thomas Nakano <nakano@mpipz.mpg.de>
2021-12-13 16:52:52 +0100

    Add files via upload

--- mafft_batch-custom.bash
+++ mafft_batch-custom.bash
@@ -1,24 +1,42 @@
 #!/bin/bash
 
+# bash script for batch-processing Sanger sequencing restuls from Eurofins
+
+# common parameters
+set -e
+set -o nounset
+dat_dir="/biodata/dep_psl/grp_psl/ThomasN/seq-results"
+
 # parameters
 ID=$1 # sequencing library (EUROFIN ID)
 
-dat_dir="/biodata/dep_psl/grp_psl/ThomasN/seq-results"
+input_file="${dat_dir}/${ID}/${ID}.fasta"
+log="${dat_dir}/${ID}/output.text"
+
+rm -rf ${log}
 
 # prepare input
-files=$(ls ${dat_dir}/${ID}/*.fasta)
+count=$(ls ${dat_dir}/${ID}/*.fasta | wc -l)
 
-cat $files > ${ID}.fasta
-/netscratch/dep_psl/grp_psl/ThomasN/tools/bin/bin/Rscript ${dat_dir}/sort_fasta.R ${dat_dir}/${ID}/${ID}.fasta
+if [ "$count" -lt 1 ]; then
+	echo "No fasta file detected."
+	exit 1
+elif [ "$count" -eq 1 ]; then
+	[ ! -f ${input_file} ] && mv ${dat_dir}/${ID}/*.fasta ${input_file}
+else
+	cat ${dat_dir}/${ID}/*.fasta > ${input_file}
+fi
+
+# sort
+/netscratch/dep_psl/grp_psl/ThomasN/tools/bin/bin/Rscript ${dat_dir}/sort_fasta.R ${input_file}
 
 # prepare fasta files
 /netscratch/dep_psl/grp_psl/ThomasN/tools/bin/bin/Rscript ${dat_dir}/mafft_batch-custom.R ${ID}
 
-# mafft
+# mafft alignment
 files=$(ls ${dat_dir}/${ID}/*temp.fasta)
-
 for f in ${files}; do
-	mafft --adjustdirection --leavegappyregion --auto ${f} > ${f/temp.fasta/.fasta}
+	mafft --adjustdirection --leavegappyregion --auto ${f} > ${f/temp.fasta/.fasta} &>> ${log}
 	rm ${f}
 done
 
